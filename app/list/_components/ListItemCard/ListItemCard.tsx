3bf0091e0e2d30291ac47711e5a3f28013735f66
WhiteDog1004 <dlrlgus1204@naver.com>
2025-03-12 18:32:53 +0900

    design : 날짜 표기 & 툴팁 추가

--- app/list/_components/ListItemCard/ListItemCard.tsx
+++ app/list/_components/ListItemCard/ListItemCard.tsx
@@ -3,8 +3,9 @@ import { getClassImages, getLabelByJobs } from '@/utils/jobs';
 import { getTextByCode } from '@/utils/mapCode';
 import { EXCHANGE_TYPE, ExchangeTypes } from '@/utils/recommendType';
 import { ManageSearch, ThumbUp } from '@mui/icons-material';
-import { Avatar, Badge, Box, Card, CardActionArea, Chip, Typography } from '@mui/material';
+import { Avatar, Badge, Box, Card, CardActionArea, Chip, Tooltip, Typography } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
+import dayjs from 'dayjs';
 import Image from 'next/image';
 import { getBadgeType } from './ListItemCard.const';
 
@@ -47,111 +48,141 @@ export const ListItemCard = ({ data }: ListItemCardProps) => {
         </Box>
       }
     >
-      <Card variant='outlined' sx={{ width: 280 }}>
-        <CardActionArea
-          sx={{
-            p: 2,
-          }}
-          className='flex flex-col gap-4 relative'
-        >
-          <Typography maxWidth={200} noWrap color='textPrimary'>
-            {data.title}
-          </Typography>
-          <Box className='flex flex-col gap-2'>
-            <Box className='flex gap-4 items-center justify-between'>
-              <Box className='w-28 h-28 relative overflow-hidden'>
-                {minimap && (
-                  <Image
-                    className='h-full object-contain'
-                    unoptimized
-                    sizes='140px'
-                    width={140}
-                    height={140}
-                    src={minimap.url}
-                    alt='minimap'
-                  />
-                )}
-              </Box>
-              <Box className='flex flex-col text-end gap-4 min-w-24'>
-                <Box className='flex flex-col gap-1'>
-                  <Box className='flex gap-1 items-center justify-end'>
-                    <Image
-                      width={20}
-                      height={20}
-                      unoptimized
-                      src={`/images/class/${getClassImages(
-                        getLabelByJobs(data.job)?.id || 0
-                      )}.webp`}
-                      alt='class'
-                    />
-                    <Typography variant='body2'>{getLabelByJobs(data.job)?.label}</Typography>
-                  </Box>
-                  <Box className='flex gap-1 items-center justify-end'>
-                    <Typography variant='body2'>
-                      레벨 : {Math.min(...data.map_data.map((item) => item.level.min || 0))}
-                    </Typography>
-                    ~
-                    <Typography
-                      sx={{
-                        lineHeight: 1,
-                      }}
-                      variant='caption'
-                      color='textDisabled'
-                    >
-                      {Math.max(...data.map_data.map((item) => item.level.max || 0))}
-                    </Typography>
-                  </Box>
-                </Box>
-                <span className={`${getBadgeType[data.hunt_type as ExchangeTypes]} justify-center`}>
-                  <Typography variant='body2'>
-                    {EXCHANGE_TYPE[data.hunt_type as ExchangeTypes]}
-                  </Typography>
-                </span>
-              </Box>
-            </Box>
-
+      <Tooltip
+        title={lowestMap.caption}
+        followCursor
+        arrow
+        slotProps={{
+          popper: {
+            modifiers: [
+              {
+                name: 'offset',
+                options: {
+                  offset: [0, 16],
+                },
+              },
+            ],
+          },
+        }}
+      >
+        <Card variant='outlined' sx={{ width: 280 }}>
+          <CardActionArea
+            sx={{
+              p: 2,
+            }}
+            className='flex flex-col gap-4 relative'
+          >
+            <Typography maxWidth={200} noWrap color='textPrimary'>
+              {data.title}
+            </Typography>
             <Box className='flex flex-col gap-2'>
-              <Box className='flex flex-col items-center gap-2'>
-                {data.map_data.length <= 1 ? (
-                  <Typography variant='body2'>
-                    {getTextByCode(Number(lowestMap.map))?.kor?.split(':')[1]}
-                  </Typography>
-                ) : (
-                  <Box className='flex flex-row items-center gap-1 max-w-60'>
-                    <Typography noWrap variant='body2'>
+              <Box className='flex gap-4 items-center justify-between'>
+                <Box className='w-28 h-28 relative overflow-hidden'>
+                  {minimap && (
+                    <Image
+                      className='h-full object-contain'
+                      unoptimized
+                      sizes='140px'
+                      width={140}
+                      height={140}
+                      src={minimap.url}
+                      alt='minimap'
+                    />
+                  )}
+                </Box>
+                <Box className='flex flex-col text-end gap-4 min-w-24'>
+                  <Box className='flex flex-col gap-1'>
+                    <Box className='flex gap-1 items-center justify-end'>
+                      <Image
+                        width={20}
+                        height={20}
+                        unoptimized
+                        src={`/images/class/${getClassImages(
+                          getLabelByJobs(data.job)?.id || 0
+                        )}.webp`}
+                        alt='class'
+                      />
+                      <Typography variant='body2'>{getLabelByJobs(data.job)?.label}</Typography>
+                    </Box>
+                    <Box className='flex gap-1 items-center justify-end'>
+                      <Typography variant='body2'>
+                        레벨 : {Math.min(...data.map_data.map((item) => item.level.min || 0))}
+                      </Typography>
+                      ~
+                      <Typography
+                        sx={{
+                          lineHeight: 1,
+                        }}
+                        variant='caption'
+                        color='textDisabled'
+                      >
+                        {Math.max(...data.map_data.map((item) => item.level.max || 0))}
+                      </Typography>
+                    </Box>
+                  </Box>
+                  <span
+                    className={`${getBadgeType[data.hunt_type as ExchangeTypes]} justify-center`}
+                  >
+                    <Typography variant='body2'>
+                      {EXCHANGE_TYPE[data.hunt_type as ExchangeTypes]}
+                    </Typography>
+                  </span>
+                </Box>
+              </Box>
+
+              <Box className='flex flex-col gap-2'>
+                <Box className='flex flex-col items-center gap-2'>
+                  {data.map_data.length <= 1 ? (
+                    <Typography variant='body2'>
                       {getTextByCode(Number(lowestMap.map))?.kor?.split(':')[1]}
                     </Typography>
-                    <Typography variant='caption'>~</Typography>
-                    <Typography
-                      noWrap
-                      variant='caption'
-                      sx={{ lineHeight: 1 }}
-                      color='textDisabled'
-                    >
-                      {getTextByCode(Number(highestMap.map))?.kor?.split(':')[1]}
+                  ) : (
+                    <Box className='flex flex-row items-center gap-1 max-w-60'>
+                      <Typography noWrap variant='body2'>
+                        {getTextByCode(Number(lowestMap.map))?.kor?.split(':')[1]}
+                      </Typography>
+                      <Typography variant='caption'>~</Typography>
+                      <Typography
+                        noWrap
+                        variant='caption'
+                        sx={{ lineHeight: 1 }}
+                        color='textDisabled'
+                      >
+                        {getTextByCode(Number(highestMap.map))?.kor?.split(':')[1]}
+                      </Typography>
+                    </Box>
+                  )}
+                </Box>
+                <Box className='flex justify-between items-center' color='GrayText'>
+                  <Box className='flex items-end gap-2'>
+                    <Chip
+                      sx={{
+                        gap: 0.5,
+                        maxWidth: 108,
+                        color: 'GrayText',
+                      }}
+                      color='default'
+                      size='small'
+                      avatar={<Avatar alt='avatar' src='/images/mushroom.png' />}
+                      label={
+                        <Typography noWrap variant='caption'>
+                          {data.user.nickname || '익명의 모험가'}
+                        </Typography>
+                      }
+                      variant='outlined'
+                    />
+                    <Typography variant='caption' color='textDisabled'>
+                      {dayjs(data.created_at).format('YY.MM.DD')}
                     </Typography>
                   </Box>
-                )}
-              </Box>
-              <Box className='flex justify-between items-center' color='GrayText'>
-                <Chip
-                  sx={{
-                    gap: 0.5,
-                    maxWidth: 108,
-                    color: 'GrayText',
-                  }}
-                  color='default'
-                  size='small'
-                  avatar={<Avatar alt='avatar' src='/images/mushroom.png' />}
-                  label={data.user.nickname || '익명의 모험가'}
-                  variant='outlined'
-                />
-                <ManageSearch color='inherit' />
+
+                  <ManageSearch color='inherit' />
+                </Box>
               </Box>
             </Box>
-          </Box>
-        </CardActionArea>
-      </Card>
+          </CardActionArea>
+        </Card>
+      </Tooltip>
     </Badge>
   );
 };
