13744d10c6a4c9d63095c5d1bc8b008e312b87af
WhiteDog1004 <dlrlgus1204@naver.com>
2025-04-24 20:21:09 +0900

    design : listDetail 등장몬스터 없을 때 안내 문구 표시

--- app/list/[uuid]/_components/DetailContentGetMap/DetailContentGetMap.tsx
+++ app/list/[uuid]/_components/DetailContentGetMap/DetailContentGetMap.tsx
@@ -11,6 +11,7 @@ import {
   Divider,
   Paper,
   Skeleton,
+  Stack,
   Typography,
 } from '@mui/material';
 import Image from 'next/image';
@@ -85,48 +86,61 @@ export const DetailContentGetMap = ({ data: list, job }: DetailContentGetMapProp
                       </Typography>
                     </Box>
                   </Box>
-                  {(list.mobs || []).length > 0 && (
-                    <Box className='flex flex-col gap-2'>
-                      <Typography noWrap variant='body2' color='textSecondary'>
-                        등장 몬스터
+                  <Box className='flex flex-col gap-2'>
+                    <Typography noWrap variant='body2' color='textSecondary'>
+                      등장 몬스터
+                    </Typography>
+                    <Paper
+                      variant='outlined'
+                      className={
+                        (list.mobs || []).length > 0
+                          ? 'grid grid-cols-[repeat(3,1fr)] gap-y-4 justify-items-center gap-1 max-h-40 px-2 py-4 overflow-y-auto'
+                          : 'flex flex-col gap-2 p-2'
+                      }
+                    >
+                      {(list.mobs || []).length > 0 ? (
+                        <>
+                          {queries.map((mob, index) => (
+                            <Box className='w-max h-max m-h-16 max-w-16' key={index}>
+                              {mob.isLoading || isLoading ? (
+                                <Loading />
+                              ) : (
+                                <Link
+                                  href={`https://mapledb.kr/search.php?q=${
+                                    mob.data?.split('mob/')[1].split('/')[0]
+                                  }&t=mob`}
+                                  target='_blank'
+                                >
+                                  <Image
+                                    className='h-16 object-contain'
+                                    width={64}
+                                    height={64}
+                                    src={mob.data as string}
+                                    alt='mob'
+                                  />
+                                </Link>
+                              )}
+                            </Box>
+                          ))}
+                        </>
+                      ) : (
+                        <Stack alignItems='center' gap={0.5}>
+                          <Image width={140} height={58} src={'/images/die1_7.png'} alt='lycan' />
+                          <Typography variant='caption' color='textSecondary'>
+                            등장하는 몬스터가 없어요!
+                          </Typography>
+                        </Stack>
+                      )}
+                    </Paper>
+                    <Box className='flex flex-col'>
+                      <Typography variant='caption' color='textSecondary'>
+                        몬스터를 클릭하면 정보를 확인할 수 있어요!
                       </Typography>
-                      <Paper
-                        variant='outlined'
-                        className='grid grid-cols-[repeat(3,1fr)] gap-y-4 justify-items-center gap-1 max-h-40 px-2 py-4 overflow-y-auto'
-                      >
-                        {queries.map((mob, index) => (
-                          <Box className='w-max h-max m-h-16 max-w-16' key={index}>
-                            {mob.isLoading || isLoading ? (
-                              <Loading />
-                            ) : (
-                              <Link
-                                href={`https://mapledb.kr/search.php?q=${
-                                  mob.data?.split('mob/')[1].split('/')[0]
-                                }&t=mob`}
-                                target='_blank'
-                              >
-                                <Image
-                                  className='h-16 object-contain'
-                                  width={64}
-                                  height={64}
-                                  src={mob.data as string}
-                                  alt='mob'
-                                />
-                              </Link>
-                            )}
-                          </Box>
-                        ))}
-                      </Paper>
-                      <Box className='flex flex-col'>
-                        <Typography variant='caption' color='textSecondary'>
-                          몬스터를 클릭하면 정보를 확인할 수 있어요!
-                        </Typography>
-                        <Typography variant='caption' color='textDisabled'>
-                          src. 메랜디비
-                        </Typography>
-                      </Box>
+                      <Typography variant='caption' color='textDisabled'>
+                        src. 메랜디비
+                      </Typography>
                     </Box>
-                  )}
+                  </Box>
                 </Box>
               )}
             </>
