2ea3f5227af29f8b77d305affe127af3719e2e97
WhiteDog1004 <dlrlgus1204@naver.com>
2025-04-24 23:57:01 +0900

    design : 문구 수정

--- app/_components/Main/MainClientPage/MainClientPage.tsx
+++ app/_components/Main/MainClientPage/MainClientPage.tsx
@@ -2,6 +2,7 @@
 
 import { Stack, Typography, useMediaQuery } from '@mui/material';
 import Image from 'next/image';
+import Link from 'next/link';
 import { MainAddBox } from '../MainAddBox';
 import { MainListBox } from '../MainListBox';
 
@@ -10,7 +11,13 @@ export const MainClientPage = () => {
 
   return (
     <Stack alignItems='center'>
-      <Stack direction='row' gap={2} justifyContent='center' alignItems='center'>
+      <Stack
+        direction='row'
+        ml={isMobile ? 0 : -17}
+        gap={2}
+        justifyContent='center'
+        alignItems='center'
+      >
         <Image src='/images/mapleland.webp' alt='mapleland' width={120} height={80} />
         <Stack>
           <Typography variant='h4'>레벨지지</Typography>
@@ -21,9 +28,23 @@ export const MainClientPage = () => {
         <MainListBox />
         <MainAddBox />
       </Stack>
-      <Typography variant='caption' color='textDisabled'>
-        Data sourced from Maplestory.io
-      </Typography>
+      <Stack>
+        <Typography variant='caption' color='textDisabled'>
+          Data sourced from
+        </Typography>
+        <Typography variant='caption' color='textDisabled'>
+          -{' '}
+          <Link href='https://maplestory.io/' target='_blank'>
+            Maplestory.io
+          </Link>
+        </Typography>
+        <Typography textAlign='left' variant='caption' color='textDisabled'>
+          -{' '}
+          <Link href='https://mapledb.kr/' target='_blank'>
+            MapleDB
+          </Link>
+        </Typography>
+      </Stack>
     </Stack>
   );
 };
