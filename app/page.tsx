71b407f3f9663dc539078d08c3378fe9816e23e7
WhiteDog1004 <dlrlgus1204@naver.com>
2025-04-26 19:36:58 +0900

    design : 모바일 디자인 잘려서 나오는 현상 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { MainClientPage } from './_components/Main/MainClientPage';
 
 export default function Home() {
   return (
-    <div className='w-full min-h-screen pt-16 md:pt-0 h-full flex flex-row justify-center items-center'>
+    <div className='w-full min-h-screen pt-24 pb-8 md:pt-0 h-full flex flex-row justify-center items-center'>
       <MainClientPage />
     </div>
   );
