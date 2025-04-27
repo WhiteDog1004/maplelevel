f076e9c520ee6603639763fc6fa0b5e298e184a1
WhiteDog1004 <dlrlgus1204@naver.com>
2025-04-28 00:05:22 +0900

    refac : 설명 120자에서 800자로 변경

--- app/add/_components/SelectInformation/SelectInformation.const.ts
+++ app/add/_components/SelectInformation/SelectInformation.const.ts
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
 export const selectInfoSchema = yup.object().shape({
-  caption: yup.string().max(120, '최대 120자까지 입력 가능합니다.'),
+  caption: yup.string().max(800, '최대 800자까지 입력 가능합니다.'),
   place: yup.number().required('최소 하나는 선택해야 합니다'),
   type: yup.string().required('최소 하나는 선택해야 합니다'),
   minLevel: yup
