5919b92be97c53cb9842a7bac6b2c182e962f18b
WhiteDog1004 <dlrlgus1204@naver.com>
2025-04-29 18:32:54 +0900

    refac : 게시글 리스트 dynamic metadata 반영

--- app/list/[uuid]/page.tsx
+++ app/list/[uuid]/page.tsx
@@ -1,13 +1,34 @@
 import { getDetailList } from '@/actions/listActions';
-import { SITE_TITLE } from '@/utils/string';
+import { getLabelByJobs } from '@/utils/jobs';
 import { Box } from '@mui/material';
 import { Metadata } from 'next';
 import { ListDetailClientPage } from './_components/ListDetailClientPage';
 
-export const metadata: Metadata = {
-  title: `${SITE_TITLE} - 사냥터 상세`,
-  description: '메이플랜드 사냥터 상세',
-};
+export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
+  const { uuid } = await params;
+  const post = await getDetailList(uuid);
+
+  return {
+    title: `${post.title} - 레벨지지`,
+    description: `${getLabelByJobs(post.job)?.label} ${
+      post.map_data[0].level.min
+    }레벨 사냥터 - 레벨지지`,
+    openGraph: {
+      title: post.title,
+      description: `${getLabelByJobs(post.job)?.label} ${
+        post.map_data[0].level.min
+      }레벨 사냥터 - 레벨지지`,
+      images: [
+        {
+          url: 'https://maplelevel.gg/images/maplelevelgg.png',
+          width: 1200,
+          height: 630,
+          alt: '레벨지지',
+        },
+      ],
+    },
+  };
+}
 
 type Params = Promise<{ uuid: string }>;
 
