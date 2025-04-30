ccceaa9d44f212a783f2e5b747706da569f173aa
WhiteDog1004 <dlrlgus1204@naver.com>
2025-04-30 17:29:10 +0900

    fix : sitemap 수정

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,28 +1,47 @@
 import { createServerSupabaseClient } from '@/supabase/server';
-import { MetadataRoute } from 'next';
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export async function GET(): Promise<Response> {
   const supabase = await createServerSupabaseClient();
   const { data: posts } = await supabase.from('recommend-list').select('uuid, created_at');
 
-  if (!posts) return [];
+  if (!posts) return new Response('', { status: 500 });
 
-  return [
+  const urls = [
     {
-      url: 'https://www.maplelevel.gg',
-      lastModified: new Date().toISOString(),
+      loc: 'https://www.maplelevel.gg',
+      lastmod: new Date().toISOString(),
     },
     {
-      url: 'https://www.maplelevel.gg/notice',
-      lastModified: new Date().toISOString(),
+      loc: 'https://www.maplelevel.gg/notice',
+      lastmod: new Date().toISOString(),
     },
     {
-      url: 'https://www.maplelevel.gg/list',
-      lastModified: new Date().toISOString(),
+      loc: 'https://www.maplelevel.gg/list',
+      lastmod: new Date().toISOString(),
     },
     ...posts.map((post) => ({
-      url: `https://www.maplelevel.gg/list/${post.uuid}`,
-      lastModified: post.created_at,
+      loc: `https://www.maplelevel.gg/list/${post.uuid}`,
+      lastmod: post.created_at,
     })),
   ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls
+      .map(
+        (url) => `
+      <url>
+        <loc>${url.loc}</loc>
+        <lastmod>${new Date(url.lastmod).toISOString()}</lastmod>
+      </url>
+    `
+      )
+      .join('')}
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
 }
