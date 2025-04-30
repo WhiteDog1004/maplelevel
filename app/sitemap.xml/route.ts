0888f1db3cd864372e157fb2daad4620db8a82c3
WhiteDog1004 <dlrlgus1204@naver.com>
2025-04-30 17:21:12 +0900

    feat : sitemap 추가

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,20 @@
+import { createServerSupabaseClient } from '@/supabase/server';
+import { MetadataRoute } from 'next';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const supabase = await createServerSupabaseClient();
+  const { data: posts } = await supabase.from('recommend-list').select('uuid, created_at');
+
+  if (!posts) return [];
+
+  return [
+    {
+      url: 'https://www.maplelevel.gg/list',
+      lastModified: new Date().toISOString(),
+    },
+    ...posts.map((post) => ({
+      url: `https://www.maplelevel.gg/list/${post.uuid}`,
+      lastModified: post.created_at,
+    })),
+  ];
+}
