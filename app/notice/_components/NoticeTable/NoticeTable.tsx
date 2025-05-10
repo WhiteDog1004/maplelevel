0c9e88768a468bed3b0f0bb87e4445f08269c539
WhiteDog1004 <dlrlgus1204@naver.com>
2025-05-11 07:29:26 +0900

    design : 공지사항 api 호출중에 로딩표시

--- app/notice/_components/NoticeTable/NoticeTable.tsx
+++ app/notice/_components/NoticeTable/NoticeTable.tsx
@@ -1,5 +1,7 @@
+import { Loading } from '@/app/_components/Loading';
 import { useGetNotice } from '@/hooks/api/useGetNotice/useGetNotice';
 import {
+  Box,
   Paper,
   Table,
   TableBody,
@@ -12,36 +14,42 @@ import dayjs from 'dayjs';
 import Link from 'next/link';
 
 export const NoticeTable = () => {
-  const { data } = useGetNotice();
+  const { data, isLoading } = useGetNotice();
 
   return (
     <TableContainer component={Paper} className='max-w-3xl w-full mx-auto my-8'>
-      <Table>
-        <TableHead sx={{ bgcolor: 'ThreeDDarkShadow' }}>
-          <TableRow>
-            <TableCell width={60} align='center'>
-              번호
-            </TableCell>
-            <TableCell sx={{ fontWeight: 'bold' }}>제목</TableCell>
-            <TableCell width={120} align='center' sx={{ fontWeight: 'bold' }}>
-              작성일
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data?.map((notice) => (
-            <TableRow key={notice.id} hover>
-              <TableCell align='center'>{notice.id}</TableCell>
-              <TableCell>
-                <Link href={`/notice/${notice.uuid}`} className='hover:underline'>
-                  {notice.title}
-                </Link>
+      {isLoading ? (
+        <Box py={2}>
+          <Loading />
+        </Box>
+      ) : (
+        <Table>
+          <TableHead sx={{ bgcolor: 'ThreeDDarkShadow' }}>
+            <TableRow>
+              <TableCell width={60} align='center'>
+                번호
               </TableCell>
-              <TableCell align='center'>{dayjs(notice.created_at).format('YY-MM-DD')}</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }}>제목</TableCell>
+              <TableCell width={120} align='center' sx={{ fontWeight: 'bold' }}>
+                작성일
+              </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {data?.map((notice) => (
+              <TableRow key={notice.id} hover>
+                <TableCell align='center'>{notice.id}</TableCell>
+                <TableCell>
+                  <Link href={`/notice/${notice.uuid}`} className='hover:underline'>
+                    {notice.title}
+                  </Link>
+                </TableCell>
+                <TableCell align='center'>{dayjs(notice.created_at).format('YY-MM-DD')}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </TableContainer>
   );
 };
