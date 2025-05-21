988da4b0d661168e5d2542077940bea719e33761
WhiteDog1004 <dlrlgus1204@naver.com>
2025-05-21 21:10:26 +0900

    design : 실서버 디자인이 이상하게 적용되던 현상 수정

--- app/quick-search/_components/QuickSearchClientPage/QuickSearchClientPage.tsx
+++ app/quick-search/_components/QuickSearchClientPage/QuickSearchClientPage.tsx
@@ -8,7 +8,7 @@ import { SearchMapCard } from '../SearchMapCard';
 export const QuickSearchClientPage = () => {
   const [mapCode, setMapCode] = useState('');
   return (
-    <Stack px={2} gap={2} width='max-content'>
+    <Stack px={2} gap={2} maxWidth={480} width='100%'>
       <SearchMapCard setMapCode={setMapCode} />
       <ResultJobsBox mapCode={mapCode} />
     </Stack>
